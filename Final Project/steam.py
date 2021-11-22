81f8ac9f97f5c2f92d2ab97239e61d4a18d5f183
LissetteMariePortfolio <89919777+LissetteMariePortfolio@users.noreply.github.com>
2021-11-22 16:56:48 -0500

    initial files uploaded

---
+++ Final Project/steam.py
@@ -0,0 +1,40 @@
+def api_bs_setup():
+    #Access API / Set up access
+    #Set up database 
+    #Make cur object
+    #Make con object
+    #Set up soup
+    pass
+def access_api():
+    #Access API with connection
+    ##Store 100 items in the database from your API 
+    #Each time you execute the code you must only store 25 or fewer items, 
+    # run the code multiple times to get 100 items total processed
+    pass
+def calc_top_from_category():
+    #Combine 2 tables in video game
+    #With stored items, look at their genres
+    # Make dictionary of genres and how many items fall into each
+    # find the highest number, store in json, csv or text file
+
+    pass
+
+def make_plotly_graphic(genre = "None"):
+    #Combine 2 tables in video game
+    #Using Plotly make graph visuals
+    #Try to make a different one, for now
+    #iTunes doing percentage pie of the different genres
+    #Steam: use scatter plot to find the average rating of a given genre
+    #iMDB: bar graph where you put ratings on the x axis (0-5) and how many 
+    #songs from a genre in your table have that info
+
+
+    pass
+
+#Tentative Deadline December 5th for code
+
+
+'''
+Notes:
+Need a commin column (medium?) in each table stating whether this is a game, song, or movie
+'''
