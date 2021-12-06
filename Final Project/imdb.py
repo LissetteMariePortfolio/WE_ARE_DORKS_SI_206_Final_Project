164ecfaad7d80cd8d7a3744f7f7e603b72a02d3c
kashafusman1 <kashafu@umich.edu>
2021-12-07 01:36:28 +0500

    trying to write to database

--- Final Project/imdb.py
+++ Final Project/imdb.py
@@ -12,19 +12,16 @@ def set_conn():
     conn = sqlite3.connect('imdb.db') 
     cursor = conn.cursor()
     return conn, cursor
-
 def create_table():
-    conn,c = set_conn()
+    conn,c=set_conn()
     c.execute('''CREATE TABLE IF NOT EXISTS movies(id INT PRIMARY KEY, title TEXT, run_time TEXT, genre TEXT,rating REAL)''')
     conn.commit()
     conn.close()
     return True
-
 def get_db_count():
-    conn,c = set_conn()
+    conn,c=set_conn()
     c.execute("SELECT COUNT(*) from movies")
     return c.fetchone()[0]
-
 def add_to_database(title,genre,run_time,rating):
     conn,c=set_conn()
     st="""INSERT INTO movies(title,run_time,genre,rating) VALUES (?,?,?,?);"""
@@ -33,7 +30,6 @@ def add_to_database(title,genre,run_time,rating):
     conn.commit()
     conn.close()
     return True
-
 def api_bs_setup():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -67,7 +63,6 @@ def get_data():
   conn,c=set_conn()
   c.execute("SELECT * FROM movies;")
   return c.fetchall()
-  
 def calc_top_from_category():
     data=get_data()
     genre={}
@@ -90,11 +85,20 @@ def make_plotly_graphic():
 
     fig.show()
 
-if __name__ == "__main__":
-  create_table()
-  access_api()
-  make_plotly_graphic()
+def write_to_file():
+    data=calc_top_from_category()
+    with open("file.txt","w") as file:
+        for movie in data.items():
+            file.write(f"{movie[0]} {movie[1]}\n")
 
+
+def main():
+    create_table()
+    access_api()
+    make_plotly_graphic()
+    write_to_file()
+if __name__ == "__main__":
+    main()
 '''
 Notes:
 Need a commin column (medium?) in each table stating whether this is a game, song, or movie
